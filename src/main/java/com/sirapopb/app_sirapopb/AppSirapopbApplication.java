7765d332a399c97ac91f0bc5b531676a4fbb0a91
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-03-15 17:42:51 +0700

    add mysql config

--- src/main/java/com/sirapopb/app_sirapopb/AppSirapopbApplication.java
+++ src/main/java/com/sirapopb/app_sirapopb/AppSirapopbApplication.java
@@ -1,7 +1,9 @@
 package com.sirapopb.app_sirapopb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppSirapopbApplication {
@@ -9,6 +11,12 @@ public class AppSirapopbApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(AppSirapopbApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner->{
+			System.out.println("Hello");
+		};
 	}
 
 }
